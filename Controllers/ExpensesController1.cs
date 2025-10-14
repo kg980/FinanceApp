01e7fab45d9995084cb3cd188eb79ed2fad35b16
Kinga <k.garbowska1@gmail.com>
2025-10-14 22:31:03 +0100

    added expenses migration to sql srv

---
+++ Controllers/ExpensesController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceApp.Controllers
+{
+    public class ExpensesController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Data/FinanceAppContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FinanceApp.Data
+{
+    public class FinanceAppContext : DbContext
+    {
+        public FinanceAppContext(DbContextOptions<FinanceAppContext> options):base(options) { }
+
+        DbSet<Models.Expense> Expenses { get; set; }
+
+
+    }
+}
